de82cf305741a0dd7461cadf50f1c35002c20693
britneytrxn <92898620+britneytrxn@users.noreply.github.com>
2022-03-01 16:54:08 -0800

    Update OrganizationsFor.tsx

--- app/screens/OrganizationsFor.tsx
+++ app/screens/OrganizationsFor.tsx
@@ -6,10 +6,12 @@ import { Text, View } from '../components/Themed';
 export default function TabTwoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Organizations for Animal Rights</Text>
-      <Text style={styles.description}>
-        Help aid organizations fighting for animals to be free of involvement and suffering in medical research, hunting, and other industries that benefit humans. 
-      </Text>
+      <View style={styles.upper}>
+        <Text style={styles.title}>Organizations for Animal Rights</Text>
+        <Text style={styles.description}>
+          Help aid organizations fighting for animals to be free of involvement and suffering in medical research, hunting, and other industries that benefit humans. 
+        </Text>
+      </View>
     </View>
   );
 }
@@ -19,8 +21,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#FEF9F5'
+    backgroundColor: 'white'
   },
+
+  // upper region (title and description)
+  upper: {
+    justifyContent: 'flex-start',
+  }, 
 
   title: {
     fontSize: 40,
@@ -29,6 +36,48 @@ const styles = StyleSheet.create({
   },
 
   description: {
+    fontSize: 10,
+    color: 'black',
+  },
+
+  // search bar and sort function
+  search_sort: {
+
+  },
+
+  search_bar: {
+
+  }, 
+
+  search_text: {
+
+  },
+
+  sort_button: {
+
+  }, 
+
+  sort_text: {
+
+  }, 
+
+  // lower region (orgs)
+  lower: {
+
+  },
+
+  org_button: {
+    backgroundColor: 'white',
+    shadowColor: 'black',
+  },
+
+  org_title: {
+    fontSize: 30,
+    color: 'black',
+    fontWeight: 'bold',
+  }, 
+  
+  org_description: {
     fontSize: 10,
     color: 'black',
   },
