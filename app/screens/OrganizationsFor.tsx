5d97f21baa2208a8bf06025b1e7ba48945356fe2
britneytrxn <92898620+britneytrxn@users.noreply.github.com>
2022-03-02 01:11:04 -0800

    editted OrganizationsFor

--- app/screens/OrganizationsFor.tsx
+++ app/screens/OrganizationsFor.tsx
@@ -1,18 +1,36 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, Image, SafeAreaView, Pressable } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import React, { useState } from 'react';
+import SearchBar from '../components/SearchBar';
+import TabOneScreen from './TabOneScreen';
 
 export default function TabTwoScreen() {
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.upper}>
         <Text style={styles.title}>Organizations for Animal Rights</Text>
         <Text style={styles.description}>
           Help aid organizations fighting for animals to be free of involvement and suffering in medical research, hunting, and other industries that benefit humans. 
         </Text>
       </View>
-    </View>
+      <View style={styles.search_sort}>
+        <View style={styles.search_bar}>
+          <SearchBar/>
+        </View>
+      </View>
+      <View style={styles.lower}>
+        <Pressable style={styles.org_button}> 
+        </Pressable>
+        <Pressable style={styles.org_button}> 
+        </Pressable>
+        <Pressable style={styles.org_button}> 
+        </Pressable>
+        <Pressable style={styles.org_button}> 
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -30,14 +48,19 @@ const styles = StyleSheet.create({
   }, 
 
   title: {
-    fontSize: 40,
+    fontSize: 35,
     fontWeight: 'bold',
     color: 'green',
+    textAlign: 'center',
   },
 
   description: {
-    fontSize: 10,
+    fontSize: 13,
     color: 'black',
+    paddingLeft: 30,
+    paddingBottom: 30,
+    paddingRight: 30,
+    paddingTop: 15,
   },
 
   // search bar and sort function
@@ -46,11 +69,14 @@ const styles = StyleSheet.create({
   },
 
   search_bar: {
-
-  }, 
-
-  search_text: {
-
+    height: 30,
+    width: 300,
+    shadowColor: 'gray', 
+    shadowOpacity: 0.4, 
+    shadowRadius: 5, 
+    shadowOffset: {width: -1, height: 2},
+    borderRadius: 20,
+    bottom: 10, 
   },
 
   sort_button: {
@@ -67,8 +93,15 @@ const styles = StyleSheet.create({
   },
 
   org_button: {
+    top: 30,
     backgroundColor: 'white',
-    shadowColor: 'black',
+    shadowColor: 'gray', 
+    shadowOpacity: 0.4, 
+    shadowRadius: 5, 
+    shadowOffset: {width: -1, height: 2},
+    borderRadius: 16, 
+    height: 100, 
+    width: 320, 
   },
 
   org_title: {
