ce779b2508429b652d4eb3291419804ad1d8c0c5
fernandakramer <86860877+fernandakramer@users.noreply.github.com>
2022-03-04 03:00:33 -0800

    Update ThankYou page

--- app/screens/ThankYou.tsx
+++ app/screens/ThankYou.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-import { Text, View, StyleSheet, Button, TextInput, Image } from "react-native";
+import { Text, View, StyleSheet, Button, TextInput, Image, Pressable } from "react-native";
 import colors from "../Themes/Colors";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
+
 
 export default function ThankYou() {
   const navigation = useNavigation();
@@ -9,27 +10,42 @@ export default function ThankYou() {
   return (
     <View style={styles.screen}>
       <Text style={styles.thanks_text}>Thank you for your donation!</Text>
-      <Button
-        title="See your GivingTree's progress!"
+      <Pressable
         onPress={() => navigation.navigate("Root")}
-      />
+        style={styles.button}>
+        <Text style={styles.nav_text}>See your GivingTree's progress</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    justifyContent: "space-between",
+  flex: 1,
+  justifyContent: 'center'
   },
   thanks_text: {
     fontSize: 40,
     fontWeight: "bold",
     color: colors.darkgreen,
     padding: 30,
+    textAlign: 'center'
   },
   go_home: {
     borderRadius: 15,
     maxWidth: "70%",
     backgroundColor: colors.lightblue,
   },
+  button: {
+    backgroundColor: colors.lightgreen,
+    alignSelf: 'center',
+    borderRadius: 30
+  },
+  nav_text: {
+    flexWrap: 'wrap',
+    fontSize: 20,
+    padding: 20,
+    textAlign: 'center',
+    fontWeight: 'bold'
+  }
 });
