8256e9be9cb67044da720acc1fa0c42a878965a1
fernandakramer <86860877+fernandakramer@users.noreply.github.com>
2022-03-10 11:55:01 -0800

    orgs-by-cause

--- app/data/causes.js
+++ app/data/causes.js
@@ -1,7 +1,7 @@
 const causes = [
-    {"id": 1, "cause": "Disease", "icon": require("./icons/disease.png")},
-    {"id": 2, "cause": "Animals", "icon": require("./icons/animals.png")},
-    {"id": 3, "cause": "Women's Rights", "icon": require("./icons/women.png")},
+    {"id": 1, "cause": "Current", "icon": require("./icons/clock.png")},
+    {"id": 2, "cause": "Disease", "icon": require("./icons/disease.png")},
+    {"id": 3, "cause": "Animals", "icon": require("./icons/animals.png")},
     {"id": 4, "cause": "Disaster Relief", "icon": require("./icons/disaster.png")},
     {"id": 5, "cause": "Shelter", "icon": require("./icons/shelter.png")},
     {"id": 6, "cause": "Education", "icon": require("./icons/education.png")},
